eabcb2460236fac9b92af9ca4773da7f1329b56a
teppei1994667 <teppei.engineer.667@gmail.com>
2023-12-29 14:07:23 +0900

    /のDialySaleの登録と一覧を/AddDialySaleに移動

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,5 @@
-import { CreateDialySaleForm } from "@/components/CreateDialySaleForm";
-import { DialySales } from "@/components/DialySales";
-import { DialySalesContextProvider } from "@/components/DialySalesContext";
-import { Grid, Paper, Typography } from "@mui/material";
-
 export const Home = () => {
-  return (
-    <DialySalesContextProvider>
-      <Paper className="w-screen h-screen pt-5">
-        <Grid container className="justify-center">
-          <Grid item>
-            <Typography className="text-gray-700 font-mono" variant="h3">
-              売り上げ管理
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container className="justify-center mt-9">
-          <Grid item>
-            <CreateDialySaleForm />
-          </Grid>
-        </Grid>
-
-        <Grid container className="justify-center mt-9">
-          <Grid item>
-            <DialySales />
-          </Grid>
-        </Grid>
-      </Paper>
-    </DialySalesContextProvider>
-  );
+  return <></>;
 };
 
 export default Home;
