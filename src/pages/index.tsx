1c578d611e704b963cb047ef93b6aa30560183f7
teppei1994667 <teppei.engineer.667@gmail.com>
2023-12-29 14:09:40 +0900

    /にタイトル追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
+import { Grid, Paper, Typography } from "@mui/material";
+
 export const Home = () => {
-  return <></>;
+  return (
+    <>
+      <Paper className="w-screen h-screen pt-5">
+        <Grid container className="justify-center">
+          <Grid item>
+            <Typography className="text-gray-700 font-mono" variant="h3">
+              なお家売り上げ登録
+            </Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+    </>
+  );
 };
 
 export default Home;
