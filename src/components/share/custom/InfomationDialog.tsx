87fd7cb4ddb40d769244e8bcebbc18ef8b876c87
teppei1994667 <teppei.engineer.667@gmail.com>
2024-02-11 15:29:34 +0900

    infomationDialogのUI 変更

--- src/components/share/custom/InfomationDialog.tsx
+++ src/components/share/custom/InfomationDialog.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogContent, DialogTitle, Grid, Typography } from "@mui/material";
+import { Button, Dialog, DialogContent, DialogTitle, Grid, Paper, Typography } from "@mui/material";
 import { Dispatch, ReactNode, SetStateAction, useEffect, useState } from "react";
 
 export type InfomationDialogProps = {
@@ -13,22 +13,25 @@ export const InfomationDialog = (props: InfomationDialogProps) => {
 
   //表示するDialogTitleを保持
   const [dialogTitle, setDialogTitle] = useState<ReactNode>();
+  const [borderColor, setBorderColor] = useState("");
 
   //初期表示時にinfomationKindsの値で表示するDialogTitleを変更
   useEffect(() => {
     switch (infomationKinds) {
       case "confirmation":
-        setDialogTitle(<Typography variant="h5">confirmation</Typography>);
+        setDialogTitle(<Typography variant="h4">confirmation</Typography>);
+        setBorderColor("border-stone-700");
         break;
       case "warning":
         setDialogTitle(
-          <Typography variant="h5" className="text-red-500">
+          <Typography variant="h4" className="text-red-500">
             warning
           </Typography>
         );
+        setBorderColor("border-red-600");
         break;
       default:
-        setDialogTitle(<Typography variant="h5"></Typography>);
+        setDialogTitle(<Typography variant="h4"></Typography>);
     }
   }, [infomationKinds]);
 
@@ -37,19 +40,22 @@ export const InfomationDialog = (props: InfomationDialogProps) => {
     setIsInfomationDialogOpen(false);
   };
   return (
-    <Dialog open={isInfomationDialogOpen} fullWidth maxWidth="md" sx={{ textAlign: "center" }}>
-      <DialogTitle className="border-solid border-red-900">
+    <Dialog open={isInfomationDialogOpen} fullWidth maxWidth="md" className="text-center">
+      <DialogTitle>
         <Grid container spacing={0.75} justifyContent="center">
           <Grid item>{dialogTitle}</Grid>
         </Grid>
       </DialogTitle>
-      <DialogContent className="h-52">
-        <Grid container spacing={0.75} className="justify-center">
-          <Grid item>
-            <Typography className="mt-10 whitespace-pre-wrap">{infomationMessage}</Typography>
+      <DialogContent>
+        <Paper elevation={0} className={`h-27 border border-dotted ${borderColor}`}>
+          <Grid container spacing={0.75} className="justify-center">
+            <Grid item>
+              <Typography className="my-5 whitespace-pre-wrap">{infomationMessage}</Typography>
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container spacing={0.75} className="justify-center mt-12">
+        </Paper>
+
+        <Grid container spacing={0.75} className="justify-center mt-5">
           <Grid item>
             <Button variant="outlined" onClick={handleCloseOnClick}>
               閉じる
