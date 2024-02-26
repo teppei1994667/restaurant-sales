a208e5ca62bc2f879e8164fe7e2c4d85bbaa24e4
teppei1994667 <teppei.engineer.667@gmail.com>
2024-02-26 23:18:46 +0900

    type/User.ts 作成

---
+++ src/type/User.ts
@@ -0,0 +1,19 @@
+export type SignUpParams = {
+  name: string;
+  email: string;
+  password: string;
+  passwardConfirmation: string;
+};
+
+export type User = {
+  id: number;
+  uid: string;
+  provider: string;
+  email: string;
+  name: string;
+  nickname?: string;
+  image?: string;
+  allowPasswordChange: boolean;
+  created_at: Date;
+  updated_at: Date;
+};
