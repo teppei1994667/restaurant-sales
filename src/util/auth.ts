b1fe7f5f6e75807c32c96b2541be226cc8a023cf
teppei1994667 <teppei.engineer.667@gmail.com>
2024-02-27 22:52:12 +0900

    util/auth.tsにコメント 追加

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,6 +1,7 @@
 import { SignUpParams } from "@/type/User";
 import { convertAxios } from "./convertAxios";
 
+//サインアップ
 export const signUp = (params: SignUpParams) => {
   return convertAxios.post("auth", params);
 };
