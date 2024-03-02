4149cfda637bb11193504d3cce09f55e75295898
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-02 12:31:42 +0900

    util/auth.tsにsignIn 実装

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,7 +1,12 @@
-import { SignUpParams } from "@/type/User";
+import { SignInParams, SignUpParams } from "@/type/User";
 import { convertAxios } from "./convertAxios";
 
-//サインアップ
+// サインアップ
 export const signUp = (params: SignUpParams) => {
   return convertAxios.post("auth", params);
+};
+
+// サインイン
+export const signIn = (params: SignInParams) => {
+  return convertAxios.post("auth/sign_in", params);
 };
