c72a0e8c3dcdd5f927a2a1d41897bbb7cf488ab8
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-03 23:30:00 +0900

    ルートページにHeader.tsx実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/Header";
 import { User } from "@/type/User";
 import { getCurrentUser } from "@/util/auth";
 import { Button, Grid, Paper, Typography } from "@mui/material";
@@ -6,29 +7,32 @@ import { useEffect, useState } from "react";
 
 export const Home = () => {
   return (
-    <Paper elevation={0} sx={{ height: "100vh" }}>
-      <Grid container className="justify-center mt-10">
-        <Grid item>
-          <Typography variant="h4">売り上げ管理アプリDialySalesへようこそ</Typography>
+    <>
+      <Header />
+      <Paper elevation={0} sx={{ height: "100vh" }}>
+        <Grid container className="justify-center mt-10">
+          <Grid item>
+            <Typography variant="h4">売り上げ管理アプリDialySalesへようこそ</Typography>
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid container className="justify-center mt-10">
-        <Grid item>
-          <Link href="/SignIn">
-            <Button className="mt-20" variant="outlined" sx={{ height: "70px", width: "200px" }}>
-              ログイン
-            </Button>
-          </Link>
+        <Grid container className="justify-center mt-10">
+          <Grid item>
+            <Link href="/SignIn">
+              <Button className="mt-20" variant="outlined" sx={{ height: "70px", width: "200px" }}>
+                ログイン
+              </Button>
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href="/SignUp">
+              <Button className="mt-20 ml-20" variant="outlined" sx={{ height: "70px", width: "200px" }}>
+                新規登録
+              </Button>
+            </Link>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Link href="/SignUp">
-            <Button className="mt-20 ml-20" variant="outlined" sx={{ height: "70px", width: "200px" }}>
-              新規登録
-            </Button>
-          </Link>
-        </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </>
     // shopページに再実装予定
     // <Paper elevation={0} sx={{ height: "100vh" }}>
     //   <Grid container className="justify-center">
