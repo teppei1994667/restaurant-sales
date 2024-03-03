d5698907475f0fccf49a5702803d7731ef2b2316
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-04 00:26:06 +0900

    useStyles 削除

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,6 @@
 import { AppBar, IconButton, Link, Theme, Toolbar, Typography, makeStyles } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
-// const useStyles = makeStyles((theme: Theme) => ({
-//   iconButton: {
-//     marginRight: theme.spacing(2),
-//   },
-//   title: {
-//     flexGrow: 1,
-//     textDecoration: "none",
-//     color: "inherit",
-//   },
-//   linkBtn: {
-//     textTransform: "none",
-//   },
-// }));
-
 export const Header = () => {
   return (
     <>
