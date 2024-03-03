2896aaf752a515c7e61150aa7fedb8d24c4a5644
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-03 22:40:30 +0900

    ルートページのレイアウトををユーザー認証用に変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,37 @@
+import { User } from "@/type/User";
+import { getCurrentUser } from "@/util/auth";
 import { Button, Grid, Paper, Typography } from "@mui/material";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
   return (
-    // <>
     <Paper elevation={0} sx={{ height: "100vh" }}>
-      <Grid container className="justify-center">
+      <Grid container className="justify-center mt-10">
         <Grid item>
-          <Typography className="text-gray-700 font-mono" variant="h3">
-            なお家売り上げ登録
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid container className="justify-center">
-        <Grid item>
-          <Link href="/AddDialySale">
-            <Button className="mt-20" variant="outlined" sx={{ height: "70px", width: "200px" }}>
-              売り上げ登録
-            </Button>
-          </Link>
+          <Typography variant="h4">売り上げ管理アプリDialySalesへようこそ</Typography>
         </Grid>
       </Grid>
     </Paper>
-    // </>
+    // shopページに再実装予定
+    // <Paper elevation={0} sx={{ height: "100vh" }}>
+    //   <Grid container className="justify-center">
+    //     <Grid item>
+    //       <Typography className="text-gray-700 font-mono" variant="h3">
+    //         なお家売り上げ登録
+    //       </Typography>
+    //     </Grid>
+    //   </Grid>
+    //   <Grid container className="justify-center">
+    //     <Grid item>
+    //       <Link href="/AddDialySale">
+    //         <Button className="mt-20" variant="outlined" sx={{ height: "70px", width: "200px" }}>
+    //           売り上げ登録
+    //         </Button>
+    //       </Link>
+    //     </Grid>
+    //   </Grid>
+    // </Paper>
   );
 };
 
