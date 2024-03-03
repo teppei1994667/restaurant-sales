6515be3b1626c32148dc4ab8a93e5d5745877b5d
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-03 22:06:42 +0900

    util/authにgetCurrentUser 実装

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -22,3 +22,16 @@ export const signOut = () => {
     },
   });
 };
+
+// 認証済みのユーザーを取得
+export const getCurrentUser = () => {
+  if (!Cookies.get("_access-toke") || !Cookies.get("_client") || !Cookies.get("_uid")) {
+    return convertAxios.get("auth/sign_in", {
+      headers: {
+        "access-token": Cookies.get("_access_token"),
+        client: Cookies.get("_client"),
+        uid: Cookies.get("_uid"),
+      },
+    });
+  }
+};
