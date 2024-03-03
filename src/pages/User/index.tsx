12d4827cfc959d28ce0024a14ed2be614d4bc76c
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-03 17:42:05 +0900

    Userページ 作成

---
+++ src/pages/User/index.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export const User = () => {
+  return (
+    <>
+      <Typography variant="h2">ここはユーザページです</Typography>
+    </>
+  );
+};
+
+export default User;
