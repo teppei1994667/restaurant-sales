411535fcbf71f247f90fd7fa586954f06d6c82d6
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-04 22:36:20 +0900

    Header.tsxにログイン時のみ表示されるsign outボタン実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
-import { AppBar, IconButton, Link, Theme, Toolbar, Typography, makeStyles } from "@mui/material";
+import { AppBar, Button, IconButton, Link, Theme, Toolbar, Typography, makeStyles } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useContext } from "react";
+import { AuthContext } from "@/context/AuthContext";
 
 export const Header = () => {
+  const { isLoading, isSignedIn, setIsSignedIn } = useContext(AuthContext);
+
+  const AuthButtons = () => {
+    // 認証完了後はサインアウト用のボタンを表示
+    // 未認証時は何も表示しない
+    if (!isLoading && isSignedIn) {
+      return <Button color="inherit">Sign out</Button>;
+    }
+  };
   return (
     <>
       <AppBar position="static">
@@ -12,6 +23,7 @@ export const Header = () => {
           <Link href="/">
             <Typography variant="h6">Sample</Typography>
           </Link>
+          <AuthButtons />
         </Toolbar>
       </AppBar>
     </>
