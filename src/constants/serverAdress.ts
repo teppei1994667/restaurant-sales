bfd0f49d66442ef000ddf3097e64a3b41f4a24c7
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-07 23:09:46 +0900

    constants/serverAdress.ts 修正

--- src/constants/serverAdress.ts
+++ src/constants/serverAdress.ts
@@ -1,3 +1,3 @@
-export const LOCAL_USER_ADDRESS = "http://localhost:3000/auth";
+export const LOCAL_AUTHUSER_ADDRESS = "http://localhost:3000/auth";
 
 export const LOCAL_DIALYSALES_ADDRESS = "http://localhost:3000/dialy_sales";
