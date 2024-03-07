23639c2afd90415e43e85cfde1ff6fe7803f632d
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-07 23:11:10 +0900

    util/convertAxios.ts 追加

---
+++ src/util/convertAxios.ts
@@ -0,0 +1,24 @@
+import { LOCAL_AUTHUSER_ADDRESS, LOCAL_DIALYSALES_ADDRESS } from "@/constants/serverAdress";
+import axios from "axios";
+import applyCaseMiddleware from "axios-case-converter";
+
+// ヘッダーに関してはケバブケースのままで良いので適用を無視するオプションを追加
+const options = {
+  ignoreHeaders: true,
+};
+
+//axiosによるサーバー通信時のスネークケース、キャメルケースの変換を自動化する
+export const convertAxios = applyCaseMiddleware(
+  axios.create({
+    baseURL: LOCAL_DIALYSALES_ADDRESS,
+  }),
+  options
+);
+
+//axiosによるサーバー通信時のスネークケース、キャメルケースの変換を自動化する
+export const authUserAxios = applyCaseMiddleware(
+  axios.create({
+    baseURL: LOCAL_AUTHUSER_ADDRESS,
+  }),
+  options
+);
