9756233b342b96fe25ab2f2970a412a9ba0a66ca
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-21 23:31:57 +0900

    authRedirect.tsで発生してた認証エラー解消と不要なconsole.log削除

--- src/util/authRedirect.ts
+++ src/util/authRedirect.ts
@@ -3,29 +3,19 @@ import { GetServerSideProps } from "next";
 
 export const withAuthServerSideProps = (url: string): GetServerSideProps => {
   return async (context) => {
-    const { req, res } = context;
-    console.log("req.cookies._uid", req.cookies._uid);
-    console.log("req.cookies._client", req.cookies._client);
-    console.log("req.cookies._access_token", req.cookies._access_token);
+    const { req } = context;
 
     const authHeaders = new Headers();
     authHeaders.append("Content-Type", "application/json");
     authHeaders.append("uid", req.cookies["_uid"] ? req.cookies["_uid"] : "");
     authHeaders.append("client", req.cookies["_client"] ? req.cookies["_client"] : "");
-    authHeaders.append("_access-token", req.cookies["_access-token"] ? req.cookies["_access-token"] : "");
-    console.log("uid", authHeaders.get("uid"));
-    console.log("client", authHeaders.get("client"));
-    console.log("_access-token", authHeaders.get("_access-token"));
+    authHeaders.append("access-token", req.cookies["_access-token"] ? req.cookies["_access-token"] : "");
 
     const response = await fetch(`${LOCAL_ADDRESS}/${url}`, {
       headers: authHeaders,
     });
 
-    console.log("response", response.ok);
-    console.log("response", response.status);
-
     if (!response.ok && response.status === 401) {
-      console.log("signInにリダイレクトします");
       return {
         redirect: {
           destination: "/SignIn",
