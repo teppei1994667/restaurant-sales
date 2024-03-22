2bd2748c5c74f35a8b4975398d3de55b23802a19
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-22 23:26:50 +0900

    SignInForm.tsx 作成

--- src/pages/SignIn/components/SignInForm.tsx
+++ src/pages/SignIn/components/SignInForm.tsx
@@ -1,7 +1,12 @@
 import { EmailTextField } from "@/components/share/authUser/EmailTextField";
 import { PassWordTextField } from "@/components/share/authUser/PasswordTextField";
+import { AuthContext } from "@/context/AuthContext";
+import { signIn } from "@/util/auth";
 import { Button, Grid, Paper } from "@mui/material";
+import { useContext } from "react";
 import { FormProvider, useForm } from "react-hook-form";
+import Cookies from "js-cookie";
+import router from "next/router";
 
 export type SignInForm = {
   name: string;
@@ -12,6 +17,33 @@ export type SignInForm = {
 
 export const SignInForm = () => {
   const signInForm = useForm<SignInForm>({ mode: "onSubmit", reValidateMode: "onChange" });
+  const { setIsSignedIn, setCurrentUser } = useContext(AuthContext);
+
+  const handleSignInOnClick = async () => {
+    const signInParams = {
+      email: signInForm.getValues("email"),
+      password: signInForm.getValues("password"),
+    };
+
+    try {
+      const res = await signIn(signInParams);
+      if (res.status === 200) {
+        console.log("ログイン成功");
+        Cookies.set("_access-token", res.headers["access-token"]);
+        Cookies.set("_client", res.headers["client"]);
+        Cookies.set("_uid", res.headers["uid"]);
+
+        setIsSignedIn(true);
+        setCurrentUser(res.data.data);
+
+        router.push("/User");
+      } else {
+        alert(`サインイン${res.status}エラー`);
+      }
+    } catch (err) {
+      alert("ログインサーバー通信エラー");
+    }
+  };
   return (
     <FormProvider {...signInForm}>
       <Paper elevation={0} sx={{ height: "100vh" }}>
@@ -37,7 +69,7 @@ export const SignInForm = () => {
         </Grid>
         <Grid container className="justify-center mt-10">
           <Grid item>
-            <Button variant="outlined" disabled={false}>
+            <Button variant="outlined" disabled={false} onClick={signInForm.handleSubmit(handleSignInOnClick)}>
               ログイン
             </Button>
           </Grid>
