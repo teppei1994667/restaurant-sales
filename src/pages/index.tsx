85395a228a7ffb1a84cd749ba8b355dea527f32c
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-24 23:37:54 +0900

    ルートページのデザイン、レイアウト変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,24 @@ import { Button, Grid, Paper, Typography } from "@mui/material";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import testImage from "../../public/test.jpeg";
+import Image from "next/image";
 
 export const Home = () => {
+  // ルートページはユーザー認証の有無関係なくアクセスできるためHeaderの状態を制御するためCookiesを取得
+  const accessToken = Cookies.get("_access-token");
+  const client = Cookies.get("_client");
+  const uid = Cookies.get("_uid");
+
+  // ユーザーの認証状態を保持
   const [isLoginStatus, setIsLoginStatus] = useState(false);
 
+  //Cookiesに値が
   useEffect(() => {
-    if (Cookies.get("_access-token") && Cookies.get("_client") && Cookies.get("_uid")) {
+    if (accessToken && client && uid) {
       setIsLoginStatus(true);
     }
-  }, []);
+  }, [accessToken, client, uid]);
 
   return (
     <>
@@ -26,8 +35,13 @@ export const Home = () => {
         </Grid>
         <Grid container className="justify-center">
           <Grid item>
+            <Image className="opacity-60 w-4/5 ml-8 mt-12" src={testImage} alt="テスト画像" />
+          </Grid>
+        </Grid>
+        <Grid container className="justify-center mt-16">
+          <Grid item>
             <Link href="/SignIn">
-              <Button className="text-gray-500 mt-20" variant="text" sx={{ height: "70px", width: "200px" }}>
+              <Button className="text-gray-500" variant="text" sx={{ height: "70px", width: "200px" }}>
                 ログイン
               </Button>
             </Link>
@@ -35,7 +49,7 @@ export const Home = () => {
           <Grid item>
             <Link href="/SignUp">
               <Button
-                className="text-gray-500 mt-20 ml-20"
+                className="text-gray-500 ml-20"
                 variant="text"
                 sx={{ height: "70px", width: "200px", color: "#6B7280" }}
               >
