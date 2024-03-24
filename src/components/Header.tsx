4014fa36c870fe78d05ed5e5a700040573bbc36f
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-24 09:48:58 +0900

    Header.tsx レイアウト変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Button, IconButton, Link, Theme, Toolbar, Typography, makeStyles } from "@mui/material";
+import { AppBar, Button, IconButton, Link, Toolbar, Typography, makeStyles } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
 export type HeaderProps = {
@@ -16,7 +16,7 @@ export const Header = (props: HeaderProps) => {
           <Link className="text-inherit ml-10" href="/" sx={{ flexGrow: "1" }}>
             <Typography variant="h6">Dialy Sales</Typography>
           </Link>
-          <Button color="inherit" sx={{ visibility: props.loginStatus ? "visible" : "hidden" }}>
+          <Button color="inherit" sx={{ visibility: props.loginStatus ? "visible" : "hidden", textTransform: "none" }}>
             Sign out
           </Button>
         </Toolbar>
