65e877ae47a5c47b24b4ce01baf3a1d89663b8c0
teppei1994667 <teppei.engineer.667@gmail.com>
2024-04-06 18:34:40 +0900

    不要なコメントアウト 削除

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -11,33 +11,7 @@ import Cookies from "js-cookie";
 export const getServerSideProps: GetServerSideProps = noPossibleAuthServerSideProps("users");
 
 export const User = (props: GetServerSideProps) => {
-  // サーバー送信用のメールアドレスをCookiesより取得
-  // const reqestUid = Cookies.get("_uid");
-  // const reqestAccessToken = Cookies.get("_access-token")
-  // const reqestClinet = Cookies.get("_client")
-
   console.log("props", props);
-
-  // ユーザー情報を取得
-  // const fetchUser = async () => {
-  //   try {
-  //     const res = await axios.get<UserInfo>(LOCAL_USER_ADRESS, {
-  //       headers: {
-  //         uid: Cookies.get("_uid"),
-  //         client: Cookies.get("_client"),
-  //         "access-token": Cookies.get("_access-token"),
-  //       },
-  //       params: { email: reqestUid },
-  //     });
-  //     console.log("res", res);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   fetchUser();
-  // }, []);
 
   return (
     <>
