66cd1c6857939fafb40651d1453ad57d0ec56aa1
teppei1994667 <teppei.engineer.667@gmail.com>
2024-04-07 16:14:39 +0900

    Userページ レイアウト 実装

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,22 +1,40 @@
 import { Header } from "@/components/Header";
 import { noPossibleAuthServerSideProps } from "@/util/authRedirect";
-import { Typography } from "@mui/material";
-import axios from "axios";
+import { Button, Grid, Link, Typography } from "@mui/material";
 import { GetServerSideProps } from "next";
-import { useCallback, useEffect } from "react";
-import { UserInfo } from "@/type/User";
-import { LOCAL_USER_ADRESS } from "@/constants/serverAdress";
-import Cookies from "js-cookie";
 
 export const getServerSideProps: GetServerSideProps = noPossibleAuthServerSideProps("users");
 
 export const User = (props: GetServerSideProps) => {
-  console.log("props", props);
+  console.log("User props", props);
+  const { name } = props;
 
   return (
     <>
       <Header loginStatus={true} />
-      <Typography variant="h2">{props.name}</Typography>
+      <Grid container className="justify-center mt-10">
+        <Grid item>
+          <Typography className="text-gray-500" variant="h4">
+            {name}
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container className="justify-center mt-10">
+        <Grid item>
+          <Typography className="text-gray-500" variant="h6">
+            管理店舗一覧
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container className="justify-center mt-16">
+        <Grid item>
+          <Link href="/AddDialySale">
+            <Button className="text-gray-500" variant="text" sx={{ height: "70px", width: "200px" }}>
+              なお家
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
     </>
   );
 };
