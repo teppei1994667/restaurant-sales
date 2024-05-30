89acac75f9a285e75e8e0e6612f790eb935abc5b
teppei1994667 <teppei.engineer.667@gmail.com>
2024-05-30 23:38:40 +0900

    DialySaleComparisonページの作成開始

---
+++ src/pages/DialySaleComparison/index.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export const DialySaleComparison = () => {
+  return (
+    <>
+      <Typography>売り上げデータの比較</Typography>
+    </>
+  );
+};
+
+export default DialySaleComparison;
